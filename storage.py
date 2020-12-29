25c82342cf58f52e8d59ef80e4101e2e685909e3
Gerben Venekamp <venekamp@gmail.com>
2020-12-29 08:54:32 +0100

    Initial Storage class with reading and writing
    The SRAM LDAP does not provide posixAccount attributes and when the
    destination LDAP does, these attributes need to be generated. The
    Storage class keeps track of these attributes and their values. If a
    value does not exist, it can be generated and stored in the Storage
    class.
    
    The initial version of this class supported reading and writing of data
    only. The generation of attribute values need to be implemented.

---
+++ storage.py
@@ -0,0 +1,59 @@
+import json
+
+class Storage:
+    def __init__(self, config=None):
+        self.isUpdated = False
+        self.config = config
+
+        if config is None:
+            self.data = {}
+            return
+
+        for storage_type, storage_config in config.items():
+            if storage_type == 'database':
+                self.data = self.ReadFromDB(storage_config)
+            if storage_type == 'file':
+                self.data = self.ReadFromFile(storage_config)
+
+
+    def ReadFromDB(self, config):
+        print("Database storage has not been implemented yet.")
+        return {}
+
+
+    def ReadFromFile(self, config):
+        if not config['path']:
+            return {}
+
+        try:
+            with open(config['path'], 'r') as fd:
+                return json.load(fd)
+        except IOError as e:
+            print(e)
+
+        return {}
+
+
+    def WriteData(self):
+        for storage_type, storage_config in self.config.items():
+            if storage_type == 'database':
+                self.WriteToDB(storage_config)
+            if storage_type == 'file':
+                self.WriteToFile(storage_config)
+
+
+    def WriteToDB(self, config):
+        pass
+
+
+    def WriteToFile(self, config):
+        if self.config and self.isUpdated:
+            try:
+                with open(config['path'], 'w') as fd:
+                    json.dump(self.data, fd)
+            except IOError as e:
+                print(e)
+
+
+    def getData(self):
+        return self.data
